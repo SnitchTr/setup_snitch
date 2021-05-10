e4898bd4e3c807e4e56fedc225b230cb3fa4c552
ArnauOrtega <62354394+ArnauOrtega@users.noreply.github.com>
2021-05-10 20:04:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo bionic main" -u
-sudo apt-get install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg-y
+sudo apt-get install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg -y
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
