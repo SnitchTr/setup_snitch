fdfb548aa7f61b58c9f0d5a9097fcd09a33c4d26
ArnauOrtega <62354394+ArnauOrtega@users.noreply.github.com>
2021-05-10 16:24:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 cd ~/
 sudo apt update
 sudo apt upgrade -y
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo bionic main" -u
+sudo apt-get install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg-y
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
@@ -14,12 +17,6 @@ sudo apt-get install python-catkin-tools python3-dev python3-numpy -y
 sudo apt install python-rosdep -y
 sudo rosdep init 
 rosdep update
-sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo bionic main" -u
-sudo apt-get install librealsense2-dkms -y
-sudo apt-get install librealsense2-utils -y
-sudo apt-get install librealsense2-dev -y
-sudo apt-get install librealsense2-dbg -y
 sudo apt install ros-melodic-rtabmap ros-melodic-rtabmap-ros ros-melodic-rosserial ros-melodic-rosserial-arduino ros-melodic-imu-filter-madgwick ros-melodic-librealsense2 -y
 mkdir ~/catkin_build_ws
 mkdir ~/catkin_build_ws/src
