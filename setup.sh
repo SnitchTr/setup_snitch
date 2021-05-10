5789bc237d76203d54c8b0c424acb93f4309117b
ArnauOrtega <62354394+ArnauOrtega@users.noreply.github.com>
2021-05-10 15:30:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ cd ~/
 sudo apt update
 sudo apt upgrade -y
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt install ros-melodic-desktop-full -y
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
