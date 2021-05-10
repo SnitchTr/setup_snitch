fa5cfa690ec40f535a483fb684b2215147d87b0e
ArnauOrtega <62354394+ArnauOrtega@users.noreply.github.com>
2021-05-10 16:25:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,8 +18,7 @@ sudo apt install python-rosdep -y
 sudo rosdep init 
 rosdep update
 sudo apt install ros-melodic-rtabmap ros-melodic-rtabmap-ros ros-melodic-rosserial ros-melodic-rosserial-arduino ros-melodic-imu-filter-madgwick ros-melodic-librealsense2 -y
-mkdir ~/catkin_build_ws
-mkdir ~/catkin_build_ws/src
+mkdir -p ~/catkin_build_ws/src
 cd ~/catkin_build
 catkin config -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
 catkin config --install
